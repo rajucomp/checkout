a94a0dd68d5e364631afae693d606d6bfbfdf7cf
Raju Gupta <raju17473@gmail.com>
2023-06-02 05:10:15 +0100

    Fixed unit tests

--- src/myapp/tests/test_payments.py
+++ src/myapp/tests/test_payments.py
@@ -1,18 +1,21 @@
+import json
 import uuid
 from services.payment_service import PaymentService
 from unittest import TestCase
 from unittest.mock import patch
+import requests
 
 class TestPayments(TestCase):
     @patch("services.payment_service")
     def test_payment_status_matches_success_when_payment_is_successful(self, mock_service) -> None:
         response = {
                 "status_code": 200,
-                "bank_transaction_id": str(uuid.uuid4()),
+                "bank_transaction_id": "d5c472f9-fcbd-4f37-bd7e-2f5aac8dae6e",
                 "payment_status": "SUCCESS",
                 "response": "Payment processed successfully",
             }
         mock_service.process_payment.return_value = response
+
 
         self.request = {
             "data": {
@@ -31,8 +34,10 @@ class TestPayments(TestCase):
             }
         }
 
-        response = mock_service.process_payment(self.request)
-        assert response["payment_status"] == "SUCCESS"
+        base_url = f"http://127.0.0.1:5000/v1/payments"
+
+        response = requests.post(base_url, json=self.request)
+        assert response.status_code== 200
 
     def test_payment_amount_matches_original_amount_when_payment_is_successfuk(
         self,
